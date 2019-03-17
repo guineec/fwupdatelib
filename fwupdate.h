d372e2e4f718b094cf677a2eab1f0c29e20fd98c
Cian Guinee <cian.guinee@gmail.com>
2019-03-17 18:07:58 +0000

    Make uplink allow dynamic amounts of data for class B

--- fwupdate.h
+++ fwupdate.h
@@ -8,4 +8,4 @@ uint8_t get_index(uint8_t *);
 
 void put_opcode(uint8_t *, uint8_t);
 void put_seq_num(uint8_t *, unsigned short);
-void make_uplink_packet(uint8_t *, uint8_t, unsigned short, uint8_t);
+void make_uplink_packet(uint8_t *, uint8_t, unsigned short, uint8_t *, uint8_t);
