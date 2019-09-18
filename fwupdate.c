154cbf354fdc7dc2fdcaa039222687131e4d7df7
Cian Guinee <cian.guinee@gmail.com>
2019-09-18 23:16:14 +0100

    Use bitwise properly

--- fwupdate.c
+++ fwupdate.c
@@ -1,18 +1,13 @@
 #include "fwupdate.h"
 #include <stdint.h>
 #include <math.h>
+#include <stdio.h>
 
 // Functions for extracting from downlink packets
 uint8_t get_opcode(uint8_t *pkt)
 {
-  // Get the first 4 bits of the first byte and convert to decimal
-  uint8_t acc = 0;
-  for (uint8_t i = 0; i < 4; i++)
-  {
-    uint8_t bit = (pkt[0] >> (4 + i)) & 1;
-    acc += (bit * (pow(2, i)));
-  }
-  return acc;
+  // Opcode is the first 4 bits
+  return pkt[0] >> 4;
 }
 
 uint8_t get_index(uint8_t *pkt)
@@ -24,16 +19,10 @@ uint8_t get_index(uint8_t *pkt)
 unsigned short get_seq_num(uint8_t *pkt)
 {
   // Extract the last 4 bits of the first byte as decimal
-  unsigned short acc = 0;
-  for (uint8_t i = 0; i < 4; i++)
-  {
-    uint8_t bit = pkt[0] << (4 + i);
-    bit = bit >> 7;
-    acc += bit * (pow(2, (11 - i)));
-  }
+  unsigned short ret = pkt[0] & 0x0F;
   // Add the second byte to it
-  acc += pkt[1];
-  return acc;
+  ret += pkt[1];
+  return ret;
 }
 
 // Functions for creating uplink packets
